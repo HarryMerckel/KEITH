a6bca2f02dae901971d2db7aa6c9a3affca5d492
davedude0 <harrymerckel@yahoo.co.uk>
2015-11-13 20:15:22 +0000

    KEITH Evolution code for PiWars 2015
    It's done, finally!
    Of course there will be plenty of time for fine tuning over the next few
    short weeks, but the majority of the code is here and ready for
    submission.

---
+++ PiWars 2015/arduino-receiver.ino
@@ -0,0 +1,27 @@
+#define channumber 5 //No. of PPM channels
+int value[channumber];
+
+void setup()
+{
+Serial.begin(115200); //Initialise serial interface
+pinMode(3, INPUT); //Pin 3 as input
+}
+
+void loop()
+{
+while(pulseIn(3, LOW) < 5000){} //Wait for the beginning of the frame
+
+for(int i=0; i<=channumber-1; i++)//Record channel data
+{
+value[i]=pulseIn(3, LOW);
+}
+
+for(int i=0; i<=channumber-1; i++)//Loop to print and clear all the channel readings
+{
+Serial.print(value[i]); //Print the value
+Serial.print(" ");
+value[i]=0; //Clear the value after is printed
+}
+
+Serial.println(""); //Start a new line
+}
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 KEITH
 =====
 
-All the code for KEITH at PiWars and beyond!
-  If set up in exactly the same was that KEITH has been, with bluetooth set up and a PiRoCon motor control board on top with the ultrasonic sensor switched to the alternative pin, this code *should* run perfectly well on your unlikely KEITH clone.
+All the code for KEITH at PiWars 2014, KEITH Evolution at PiWars 2015 and beyond!
+See [http://keiththerobot.uk/]!
+
